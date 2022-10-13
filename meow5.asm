517478468c6f51fceed9cc9c3da6c5187a6aef08
Dave Gauer <dave@ratfactor.com>
2022-10-12 23:02:06 -0400

    Now reading 'meow5' word definition from string!

--- meow5.asm
+++ meow5.asm
@@ -17,7 +17,9 @@ last: resb 4            ; Pointer to last defined word tail
 data_segment: resb 1024 ; We inline ("compile") here!
 here: resb 4            ; Current data_segment pointer
 meow_counter: resb 1    ; Will count the five meows
-temp_return_addr: resb 4 ; to avoid messing with stack!
+return_addr: resb 4     ; to avoid messing with stack!
+token_buffer: resb 32   ; For get-token
+input_buffer_pos: resb 4 ; Save position of read tokens
 
 ; ----------------------------------------------------------
 ; DATA - defined values
@@ -33,12 +35,20 @@ end_meow_str:
 temp_meow_name: db 'meow', 0
 temp_exit_name: db 'exit', 0
 
+; I'm pretending to get this string from an input source:
+; NOTE: This buffer will move to the BSS section when I
+; start reading real input.
+input_buffer_start:
+    db ' meow   meow meow meow meow exit', 0
+input_buffer_end:
+
 ; ----------------------------------------------------------
 ; TEXT - executable program - starting with words
 ; ----------------------------------------------------------
 section .text
 
 exit: ; exit WORD (takes exit code from stack)
+; ==================
     pop ebx ; exit code
     mov ebx, 0 ; exit with happy 0
     mov eax, SYS_EXIT
@@ -60,6 +70,7 @@ meow_tail:
     db "meow", 0
 
 inline: ; inline WORD (takes addr of tail from stack)
+; ==================
     pop esi ; tail of word to inline
     mov edi, [here]    ; destination
     mov ecx, [esi + 4] ; get len into ecx
@@ -67,13 +78,14 @@ inline: ; inline WORD (takes addr of tail from stack)
     rep movsb          ; copies from esi to esi+ecx into edi
     add edi, ecx       ; update here pointer...
     mov [here], edi    ; ...and store it
-    jmp [temp_return_addr]
+    jmp [return_addr]
 inline_tail:
     dd meow_tail ; link to prev word
     dd (inline_tail - inline)
     dd "inline", 0
 
 find:
+; ==================
     ; register use:
     ;   al  - to-find name character being checked
 	;   ebx - start of dict word's name string
@@ -110,17 +122,82 @@ find:
 
 .not_found:
     push 0   ; return 0 to indicate not found
-    jmp [temp_return_addr]
+    jmp [return_addr]
 
 .found_it:
     push edx ; return  pointer to tail of dictionary word
-    jmp [temp_return_addr]
+    jmp [return_addr]
 
 find_tail:
     dd inline_tail ; link to prev word
     dd (find_tail - find)
     dd "find", 0
 
+
+get_token:
+; ==================
+    ; Returns (on stack) either:
+    ;  * Address of null-termed token string or
+    ;  * 0 if we're out of tokens
+    ;
+    ;  * * *
+    ; Input 'faked' for now, but fairly realistic. This
+    ; will definitely be changing in various ways with real
+    ; line input, though. For example, we'll need to see if
+    ; there's _more_ input available.
+    ;  * * *
+    ;
+    mov ebx, [input_buffer_pos] ; set input read addr
+    mov edx, token_buffer       ; set output write addr
+    mov ecx, 0                  ; position index
+.get_char:
+    mov al, [ebx + ecx] ; input addr + position index
+    cmp al, 0           ; end of input?
+    je .end_of_input    ; yes
+    cmp al, ' '         ; token separator? (space)
+    jne .add_char       ; nope! get char
+    cmp ecx, 0          ; yup! do we have a token yet?
+    je .eat_space       ; no
+    jmp .return_token   ; yes, return it
+.eat_space:
+    inc ebx             ; 'eat' space by advancing input
+    jmp .get_char
+.add_char:
+    mov [edx + ecx], al ; write character
+    inc ecx             ; next character
+    jmp .get_char
+.end_of_input:
+    ; okay, now did we hit the end while gathering
+    ; a token, or did we come up empty-handed?
+    cmp ecx, 0         ; did we have anything?
+    jne .return_token  ; we have a token
+    push DWORD 0       ; empty-handed
+    jmp [return_addr]
+.return_token:
+    lea eax, [ebx + ecx]
+    mov [input_buffer_pos], eax ; save position
+    mov [edx + ecx], byte 0     ; terminate str null
+    push DWORD token_buffer     ; return str address
+    jmp [return_addr]
+get_token_tail:
+    dd find_tail ; link to prev word
+    dd (get_token_tail - get_token)
+    dd "get_token", 0
+
+; ----------------------------------------------------------
+; CALLWORD macro - for faking call/ret to word as if 'twas
+; a function. Will only be needed for tiny subset of words.
+; ----------------------------------------------------------
+%macro CALLWORD 1 ; takes label of word to call
+    ; This single return address will surely need to be
+    ; upgraded to a stack as soon as I 'call' a word from
+    ; another word (only here in assembly - this has nothing
+    ; to do with normal word execution!)
+    ; Note that '%%return_to' is a macro-local label.
+        mov dword [return_addr], %%return_to ; CALLWORD
+        jmp %1                               ; CALLWORD
+    %%return_to:                             ; CALLWORD
+%endmacro
 
 ; ----------------------------------------------------------
 ; PROGRAM START!
@@ -138,33 +215,41 @@ _start:
 	; dictionary at the moment).
     mov dword [last], find_tail
 
-	; ----------------------------------------------------
-    ; "Compile" the program: inline five meows and an exit.
-	; ----------------------------------------------------
-    mov byte [meow_counter], 5 ; 5 meows
-inline_a_meow:
-    ; use 'find' to get meow_tail!
-	; TEMP: ignoring a possible null pointer return because
-	; in this test I KNOW it will be found.
-	; NOTE: I'm currently leaking four bytes of memory per
-	; find because I'm not popping the param I push on the
-	; stack...
-    push temp_meow_name ; the name string to find
-    mov dword [temp_return_addr], t1
-    jmp find
-t1: mov dword [temp_return_addr], t2
-    jmp inline
-t2: dec byte [meow_counter]
-    jnz inline_a_meow
+    ; This will probably _really_ get set when we read
+    ; more input. But for now, set to start of buffer:
+    mov dword [input_buffer_pos], input_buffer_start
 
-    ; inline exit
-    push temp_exit_name ; the name string to find
-    mov dword [temp_return_addr], t3
-    jmp find
-t3: mov dword [temp_return_addr], t4
-    jmp inline
-t4:
-    ; Run!
+	; ----------------------------------------------------
+    ; "Compile" the program!
+    ; ----------------------------------------------------
+get_next_token:
+    CALLWORD get_token
+    cmp DWORD [esp], 0  ; check return without popping
+    je run_it           ; all out of tokens!
+    CALLWORD find       ; find token (ignore error)
+    CALLWORD inline     ; inline it!!!
+    jmp get_next_token
+
+;    mov byte [meow_counter], 5 ; 5 meows
+;inline_a_meow:
+;    ; use 'find' to get meow_tail!
+;	; TEMP: ignoring a possible null pointer return because
+;	; in this test I KNOW it will be found.
+;	; NOTE: I'm currently leaking four bytes of memory per
+;	; find because I'm not popping the param I push on the
+;	; stack...
+;    push temp_meow_name ; the name string to find
+;    CALLWORD find
+;    CALLWORD inline
+;    dec byte [meow_counter]
+;    jnz inline_a_meow
+;
+;    ; inline exit
+;    push temp_exit_name ; the name string to find
+;    CALLWORD find
+;    CALLWORD inline
+
+run_it:
     push 0           ; push exit code to stack for exit
     jmp data_segment ; jump to the "compiled" program
 
